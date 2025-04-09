5bd17ac698274090ac62409b65640a24dfcaec84
ayobalima <ayo.balima@ashesi.edu.gh>
2025-04-09 03:32:30 +0000

    updated
    updated the backend link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // /const API_BASE_URL = "https://student-id-system-backend.onrender.com";
-const API_BASE_URL = "http://localhost:8000";
+// const API_BASE_URL = "http://localhost:8000";
 // const API_BASE_URL = "https://f9fa-154-65-20-57.ngrok-free.app";
+const API_BASE_URL = "http://34.72.198.201:8000";
 
 export { API_BASE_URL };
